70092f0987801accc62b1a9921271a52256a7c56
hongni <hong.ni@vanderbilt.edu>
2016-01-26 16:03:06 -0600

    added PYTHIA8_TuneCUETP8M1_5TeV_OmegaFilter_cff.py

---
+++ PYTHIA8_TuneCUETP8M1_5TeV_OmegaFilter_cff.py
@@ -0,0 +1,39 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    crossSection = cms.untracked.double(71.39e+09),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(5020.0),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'SoftQCD:nonDiffractive = on',
+            'SoftQCD:singleDiffractive = on',
+            'SoftQCD:doubleDiffractive = on'),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+        )
+                         )
+
+
+omegafilter = cms.EDFilter("PythiaFilter",
+    Status = cms.untracked.int32(1),
+    MaxEta = cms.untracked.double(2.5),
+    MinEta = cms.untracked.double(-2.5),
+    MinPt = cms.untracked.double(2.4),
+    ParticleID = cms.untracked.int32(3334)
+)
+
+
+ProductionFilterSequence = cms.Sequence(generator*omegafilter)
